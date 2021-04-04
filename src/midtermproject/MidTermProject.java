87d5366216ae2237dcd5b8df5573521ae5ad4b2d
Nabeel Yousaf <n.y.562448@gmail.com>
2021-04-04 15:11:44 +0500

    Making GUI

---
+++ src/midtermproject/MidTermProject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermproject;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class MidTermProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frontPage fp = new frontPage();
+        fp.setVisible(true);
+    }
+    
+}
