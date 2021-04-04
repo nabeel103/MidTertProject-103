720aaa63190aeb26bf81a47c03cd0d0f06947e24
Nabeel Yousaf <n.y.562448@gmail.com>
2021-04-04 21:13:48 +0500

    2 Phases Done

--- src/midtermproject/LoginPage.java
+++ src/midtermproject/LoginPage.java
@@ -241,6 +241,8 @@ public class LoginPage extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
         admin a = new admin();
+        Student s = new Student();
+        s.setVisible(true);
         a.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
